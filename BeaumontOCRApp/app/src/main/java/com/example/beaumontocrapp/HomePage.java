7921425cd8581c7c76a822a775ec271468ba2729
heliu <heliu@tcd.ie>
2021-04-07 18:03:02 +0800

    Added log out function

--- BeaumontOCRApp/app/src/main/java/com/example/beaumontocrapp/HomePage.java
+++ BeaumontOCRApp/app/src/main/java/com/example/beaumontocrapp/HomePage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -16,15 +17,23 @@ public class HomePage extends AppCompatActivity {
 
     public static final String EXTRA_STRING = "com.android.beaumontocrapp.EXTRA_STRING";
 
-    private Button captureButton, howToButton, analyticsButton;
+    private Button logoutButton, captureButton, howToButton, analyticsButton;
+    private SharedPreferences mSharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
+        mSharedPreferences = getSharedPreferences("auth",MODE_PRIVATE);
+        logoutButton = (Button) findViewById(R.id.logout);
         captureButton = (Button) findViewById(R.id.captureButton);
         howToButton = (Button) findViewById(R.id.howToButton);
         analyticsButton = (Button) findViewById(R.id.analyticsButton);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { logOut(); }
+        });
 
         captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +49,7 @@ public class HomePage extends AppCompatActivity {
             }
         });
 
+
     }
 
     private void openHowTo() {
@@ -49,6 +59,15 @@ public class HomePage extends AppCompatActivity {
 
     private void doCameraActivity() {
         Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    private void logOut(){
+        SharedPreferences.Editor auth = mSharedPreferences.edit();
+        auth.putString("username","");
+        auth.putString("password","");
+        auth.apply();
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
